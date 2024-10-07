6c5fe6c9e57b4467534ca5ad0d159d9d1e5f2cd3
IRONAGE-Park <iron@invaiz.com>
2024-10-07 11:34:58 +0900

    # UPDATE: pdf_extract new version [complete]

--- src/main.rs
+++ src/main.rs
@@ -10,52 +10,55 @@ fn main() {
 
     let query = args.get(1).expect("No query provided");
 
-    println!("질의문: {query}");
-
     let mut schema_builder = Schema::builder();
 
-    let title = schema_builder.add_text_field("title", TEXT | STORED);
+    let filepath = schema_builder.add_text_field("filepath", TEXT | STORED);
     let body = schema_builder.add_text_field("body", TEXT | STORED);
     let schema = schema_builder.build();
 
     // let index = Index::create_in_dir("./index", schema.clone()).expect("Failed to create index");
-
     let index = Index::open_in_dir("./index").expect("Failed to open index");
-    // let mut index_writer: IndexWriter = index
-    //     .writer(1_000_000_000)
-    //     .expect("Failed to create index writer");
-    // let files = windows_api::search_local_files_by_query(String::from("pdf"))
-    //     .expect("Failed to search files");
-    // files.iter().for_each(|(name, path, size)| {
-    //     println!("size: {}", size);
-    //     let path = std::path::Path::new(path.strip_prefix("file:").unwrap());
-    //     println!("reading: {}", std::fs::read(path).unwrap().len());
-    //     let result =
-    //         std::panic::catch_unwind(|| pdf_extract::extract_text(path).unwrap()).map(|text| {
-    //             index_writer
-    //                 .add_document(doc!(
-    //                     title => name.clone(),
-    //                     body => text
-    //                 ))
-    //                 .expect("Failed to add document");
-    //         });
+    let mut index_writer: IndexWriter = index
+        .writer(1_000_000_000)
+        .expect("Failed to create index writer");
+    let files = windows_api::search_local_files_by_query(query.to_string())
+        .expect("Failed to search files");
+    println!("files {:?}", files.len());
+    println!("질의문: {query}");
+    files.iter().for_each(|(name, path, size)| {
+        println!("name: {}, size: {}", name, size);
+        let path = std::path::Path::new(path.strip_prefix("file:").unwrap());
+        let result =
+            std::panic::catch_unwind(|| pdf_extract::extract_text(path).map_err(|e| e.to_string()))
+                .unwrap_or_else(|e| {
+                    println!("Failed to extract text from {}: {:?}", name, e);
+                    Err("Catched panic by pdf_extract".to_string())
+                });
+        let result = result.map(|text| {
+            index_writer
+                .add_document(doc!(
+                    filepath => path.to_str().unwrap_or(""),
+                    body => text
+                ))
+                .expect("Failed to add document");
+        });
 
-    //     match result {
-    //         Ok(_) => {
-    //             println!("Added: {:?}", path);
-    //         }
-    //         Err(_) => {
-    //             println!("Failed to extract text from {}", name);
-    //             return;
-    //         }
-    //     }
-    // });
-    // println!("Committing {} files...", files.len());
-    // index_writer.commit().expect("Failed to commit");
+        match result {
+            Ok(_) => {
+                println!("[SUCCESS]: {:?}", path);
+            }
+            Err(_) => {
+                println!("[FAILURE] extract text from {}", name);
+                return;
+            }
+        }
+    });
+    println!("Committing {} files...", files.len());
+    index_writer.commit().expect("Failed to commit");
 
     let reader = index.reader().expect("Failed to create reader");
     let searcher = reader.searcher();
-    let query_parser = QueryParser::for_index(&index, vec![title, body]);
+    let query_parser = QueryParser::for_index(&index, vec![filepath, body]);
 
     let query = query_parser
         .parse_query(query)
