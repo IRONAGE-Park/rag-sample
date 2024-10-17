df50028ab022229b2b6a49c706aaae7e740efb9a
IRONAGE-Park <iron@invaiz.com>
2024-10-17 11:17:12 +0900

    # UPDATE: seperate Image caption and ocr [complete]

--- src-python/core/loader/image.py
+++ src-python/core/loader/image.py
@@ -7,5 +7,9 @@ from core.blip.blip import captioning
 def get_document_from_image(file_path: str) -> List[Document]:
   caption = captioning(file_path)
   ocr = pytesseract.image_to_string(file_path)
-  doc = [Document(page_content=f"caption: {caption}\nocr: {ocr}", metadata={ "image_path": file_path })]
+  doc: List[Document] = []
+  if caption:
+    doc.append(Document(page_content=caption, metadata={"image_path": file_path, "type": "caption"}))
+  if ocr:
+    doc.append(Document(page_content=ocr, metadata={"image_path": file_path, "type": "ocr"}))
   return doc
